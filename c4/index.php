ee3fb01e5875203da2e45fbd97574b8f9b7c8eef
prothesbarai <aspprothesbarai1619@gmail.com>
2025-03-25 17:31:39 +0600

    v1.2.1

--- c4/index.php
+++ c4/index.php
@@ -11,5 +11,24 @@
         <h1>PHP Constant</h1>
     </center>
 
+    <?php
+        // Here Variable Value Changable And Priority Purpose variable Printed
+        $v1 = "Prothes";
+        echo "$v1 ";
+        $v1 = "Barai";
+        echo $v1;
+        echo "<br>";
+
+        // Here constant value
+        define("key","Value");
+        define("keys","Prothes");
+        $age = 26;
+        echo key;
+        echo "<br>";
+        echo keys."<br>";
+        echo "My Name is : ".keys.". And My age is : ".$age;
+        
+    ?>
+
 </body>
 </html>
