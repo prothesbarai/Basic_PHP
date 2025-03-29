9b6744838a753ebe2c16dc995ebf4ad336b9fdaa
prothesbarai <aspprothesbarai1619@gmail.com>
2025-03-29 10:19:27 +0600

    v1.2.2.1

--- c7/index.php
+++ c7/index.php
@@ -37,20 +37,33 @@
 
 
     <?php
-        $car_name = "Tesla";
+        $car_name = "tTR500";
         $car_price = "$1300 USD";
-
         /*
             Difference Between echo and printf()
         */
-
         echo "For echo : "."The car name is ".$car_name.". And this car price is : ".$car_price."<br>";
-        printf("For printf : The car name is %s . And this car price is : %s",$car_name,$car_price);
-
-
+        printf("For printf : The car name is %s . And this car price is : %s, and it's provide is %s company",ucwords($car_name),$car_price,"Tesla");
     ?>
 
+        
+    <?php
+        /*
+            echo deya only akta veriable print kora jay but function with variable print possiable noy cause echo function 
+            name ke simple string count kore... otherhand printf() er majhe function with variable use kora jay.. and %s for 
+            string %d for int %f for double use kore....[%s,%d,%f = name is placeholder]
+        */
 
+        // Here a Big Example
+        $first_name = "prothes";
+        $last_name = "barai";
+        $show_names = "
+            <div>
+                <h1>%s %s</h1>
+            </div>
+        ";
+        printf($show_names,ucwords($first_name),ucwords($last_name));
+    ?>
 
 
 
